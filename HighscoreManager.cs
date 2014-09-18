e0d9d46ca81b4d8aec35206ac26a476f45789fae
Sebastian Vråle <seb@terminator.net>
2014-09-18 11:46:42 +0200

    Highscores

---
+++ HighscoreManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class HighscoreManager : MonoBehaviour {
+
+	HighScore hs;
+	public InputField InpName;
+
+	void Awake() {
+		hs = GetComponent<HighScore>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PostHighscore() {
+		if (InpName.value == "") {
+			return;
+		}
+		hs.StartCoroutine(hs.PostScores(InpName.value, 2));
+	}
+}
